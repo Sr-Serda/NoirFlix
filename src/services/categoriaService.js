ab59b92f06cd009cd885dcd247abaa41e6b07ca0
Gustavo <moreirasilvagustavo13@gmail.com>
2024-10-07 11:05:26 -0300

    Configurando Netlify

--- src/services/categoriaService.js
+++ src/services/categoriaService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api-filmes-ofc.vercel.app/categorias';
+const API_URL = 'https://api-filmes-cukex05lj-sr-serdas-projects.vercel.app/categorias';
 
 export const getCategorias = async () => {
   try {
